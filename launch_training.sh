3714ca1cbcf3db258970f82928f86782b57e5928
kcrampon <kevin.crampon@univ-reims.fr>
2022-12-04 18:42:53 +0100

    fix nvprof dir creation

--- launch_training.sh
+++ launch_training.sh
@@ -27,6 +27,7 @@ do
     model=$(echo $cmd | sed "s/--model_name /@/" | cut -d'@' -f2)
     python extract_mem_copy.py --nb_gpus $nb_devices --gbs $bs --model $model
     nvprof_dir_j=$nvprof_dir"_"$j
+    mkdir $nvprof_dir_j
     mv nvprof_out_*.csv $nvprof_dir_j/.
   done
 done
