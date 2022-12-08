53fd3bfd95ed746985c0f2926b6fff33e27b3d5e
kcrampon <kevin.crampon@univ-reims.fr>
2022-12-08 18:24:01 +0100

    Set out filename

--- launch_training.sh
+++ launch_training.sh
@@ -10,9 +10,12 @@ for i in $( seq 1 $NB_CMDS)
 do
   cmd=$(head -n $i $FILENAME| tail -1)
   current_date=$(date)
+  nb_devices=$(echo $cmd | sed "s/--nb_devices /@/" | sed "s/ --model_name/@/" | cut -d'@' -f2)
+  bs=$(echo $cmd | sed "s/--batch_size /@/" | sed "s/ --accelerator/@/" | cut -d'@' -f2)
+  model=$(echo $cmd | sed "s/--model_name /@/" | cut -d'@' -f2)
   echo "$i - $current_date - $cmd">> $ETA_FILENAME
   cmd_tmp=${cmd//[ ]/_}
-  filename="$DIR_NAME/"${cmd_tmp//[.]/_}".csv"
+  filename="$DIR_NAME/RES_--batch_size_$bs_--nb_devices_$nb_devices_--model_$model.csv"
   #nvprof_dir="$DIR_NAME/"${cmd_tmp//[.]/_}
   echo "train_time,loss,r2,rp" >> $filename
   for j in $( seq 1 $NB_TRAINING)
@@ -22,9 +25,6 @@ do
     eval $cmd &> tmp.txt
     perfs=$(tail -n 1 tmp.txt)
     echo $perfs >> $filename
-    nb_devices=$(echo $cmd | sed "s/--nb_devices /@/" | sed "s/ --model_name/@/" | cut -d'@' -f2)
-    bs=$(echo $cmd | sed "s/--batch_size /@/" | sed "s/ --accelerator/@/" | cut -d'@' -f2)
-    model=$(echo $cmd | sed "s/--model_name /@/" | cut -d'@' -f2)
     python extract_mem_copy.py --nb_gpus $nb_devices --gbs $bs --model $model
     # nvprof_dir_j=$nvprof_dir"_"$j
     # mkdir $nvprof_dir_j
